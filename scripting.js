408535f801e8e3f16e7e0165791f9b45b0d9fe8d
Robert <NRobertDavis@gmail.com>
2019-10-02 15:53:50 -0400

    Finish Project

--- scripting.js
+++ scripting.js
@@ -1,11 +1,47 @@
-function drawBox() {
-	document.getElementById('main-container').insertAdjacentHTML('beforeend', '<div id = "inner-div"></div>');
-}
+let container = document.querySelector('.container');
 
-function makeBoxes() {
-	for (let i = 0; i < 256; i++) {
-		drawBox();
-		console.log('Boxes Drawn');
-		document.getElementById('inner-div').style.border = '1px solid black';
+function createSketch(x) {
+	for (let i = 0; i < x * x; i++) {
+		let drawSpace = document.createElement('div');
+		drawSpace.classList.add('squares');
+		drawSpace.addEventListener('mouseover', hover);
+		document.querySelector('.container').appendChild(drawSpace);
+		document.documentElement.style.setProperty('--colNum', x);
+
+		function hover() {
+			drawSpace.classList.add('hover');
+		}
 	}
 }
+
+function clearBoard() {
+	let gridCount = document.querySelector('.container').childElementCount;
+	for (let j = 0; j < gridCount; j++) {
+		document.querySelector('.container').removeChild(document.querySelector('.container').children[0]);
+	}
+}
+
+function sizeChoice() {
+	let newSize = prompt('How many squares sould the grod have on each side? (max 100)');
+	clearBoard();
+	createSketch(newSize);
+}
+
+// Deal with this later
+function clearGrid() {
+	let clearSquares = document.querySelectorAll('.container div');
+	console.log('Grid is being cleared');
+	clearSquares.forEach((clearSquares) => {
+		clearSquares.classList.remove('hover');
+	});
+}
+//This is a test to see if event listeners are being added to any function or number
+window.addEventListener('click', () => {
+	console.log('Window was clicked.');
+});
+
+document.querySelector('#clear').addEventListener('click', clearGrid);
+document.querySelector('#newGrid').addEventListener('click', createSketch(sizeChoice()));
+
+//addEventListener('click', clearGrid);
+//document.querySelector('#clear').addEventListener('click', clearGrid);
